f5d102e5bd09edb847f7548f838d9c34ff120d4a
skillnull <skillisnull@gmail.com>
2020-09-23 19:45:23 +0800

    feat: update

--- src/shims.d.ts
+++ src/shims.d.ts
@@ -1,10 +1,5 @@
 import Vue from "vue"
 
 declare module "*.vue" {
-  interface WithRender {
-    <V extends typeof Vue>(component: V): V
-  }
-
-  const WithRender: WithRender
-  export default WithRender
+  export default Vue
 }
