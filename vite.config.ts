3a9c60d76f740770a2f616ebb3a9e8a03672dae6
skillnull <skillisnull@gmail.com>
2020-09-24 16:55:57 +0800

    feat: update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,37 @@
+const fs = require('fs')
+const dotenv = require('dotenv')
+const envFiles = [
+  /** default file */ `.env`,
+  /** mode file */ `.env.${ process.env.NODE_ENV }`
+]
+for (const file of envFiles) {
+  const envConfig = dotenv.parse(fs.readFileSync(file))
+  for (const k in envConfig) {
+    process.env[k] = envConfig[k]
+  }
+}
+
 module.exports = {
   alias: {},
-  hostname: '0.0.0.0',
-  port: 8080,
+  hostname: process.env.VITE_HOST,
+  port: process.env.VITE_PORT,
   // 是否自动在浏览器打开
   open: false,
   // 是否开启 https
   https: false,
   // 服务端渲染
-  ssr: true,
+  ssr: false,
+  /**
+   * Base public path when served in production.
+   * @default '/'
+   */
+  base: process.env.VITE_BASE_URL,
+  /**
+   * Directory relative from `root` where build output will be placed. If the
+   * directory exists, it will be removed before the build.
+   * @default 'dist'
+   */
+  outDir: process.env.VITE_OUTPUT_DIR,
   // 反向代理
   proxy: {
     'api': {
